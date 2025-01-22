30e067b26ed6803d78674a6a96d9a900555bc268
bhanusunrise <pasindubhanukagood@gmail.com>
2025-01-23 03:51:34 +0530

    3 components addded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,21 @@ export default function Home() {
         <div className="mt-10">
           <Button text="Learn More" bgColor="bg-green-500" hoverColor="hover:bg-green-600" focusColor="focus:ring-green-300" />
         </div>
+    </div>
+
+    <div className="columns-3 pl-40 pr-40 text-center">
+          <div className="column bg-white/10 p-10 rounded-3xl hover:bg-gray-800/50">
+            <p className="text-2xl font-semibold text-white">Cultural Diversity</p>
+            <p className="text-lg text-white">A rich multicultural heritage and traditions.</p>
+          </div>
+          <div className="column bg-white/10 p-10 rounded-3xl hover:bg-gray-800/50">
+            <p className="text-2xl font-semibold text-white">Investment Potential</p>
+            <p className="text-lg text-white">Invest in a thriving economy with endless possibilities.</p>
+          </div>
+          <div className="column bg-white/10 p-10 rounded-3xl hover:bg-gray-800/50">
+            <p className="text-2xl font-semibold text-white">Higher Studies</p>
+            <p className="text-lg text-white">Study in world-class institutions and vibrant cities.</p>
+          </div>
       </div>
       
     </>
