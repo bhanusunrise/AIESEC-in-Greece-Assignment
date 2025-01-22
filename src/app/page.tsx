744e42bd0b26e5c2648155215bd3282202ad0b4c
bhanusunrise <pasindubhanukagood@gmail.com>
2025-01-22 23:32:06 +0530

    titles added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <>
     <NavBar/>
+    <p className="text-7xl font-bold text-center">Discover Australia&apos;s <br/> Opportunities and <br/> Beauty</p>
+    <p className="text-lg text-center">Explore our culture, education, and investment potential.</p>
       
     </>
   );
